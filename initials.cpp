31d95698212a241826f24d366a1dcd07aa0ad5ba
Fernando Bitti Loureiro <fernando.bitti@gmail.com>
2018-12-09 14:01:27 +0100

    solutions

---
+++ initials.cpp
@@ -0,0 +1,22 @@
+
+// Fun fact: I like blockchain and genetics
+
+// Objective: respond the challenge with a code
+// that lets you print any text in block letters.
+
+// This version is for the initials without array, loops, functions etc.
+
+#include <iostream>
+#include <string>
+
+int main() {
+
+  std::cout << "FFFFF  BBBB   L    \n";
+  std::cout << "F      B   B  L    \n";
+  std::cout << "F      B   B  L    \n";
+  std::cout << "FFF    BBBB   L    \n";
+  std::cout << "F      B   B  L    \n";
+  std::cout << "F      B   B  L    \n";
+  std::cout << "F      BBBB   LLLLL\n";
+
+}
